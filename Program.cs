b9662cd6a4096e43c654ad9750a1ec8a4b2c6aba
Vladislav Setchin <vlad@setchinfc.com.au>
2018-06-21 23:20:26 +1000

    Sample dotnet core console app with nlog

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace nlog_console
+{
+    class Program
+    {
+        private static NLog.Logger log = NLog.LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            log.Trace("Loggin trace.");
+        }
+    }
+}
